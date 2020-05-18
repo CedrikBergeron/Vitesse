157509faaf31b02c4f7d733558ca073fcf8260e4
CedrikBergeron <cedrik.bergeron@me.com>
2020-05-18 02:47:03 -0400

    Aucune carte n'apparait 2 fois lors de la partie, la carte sélectionnée est afficher en haut du jeu et est remplacée par une nouvelle

--- src/main/java/Vitesse_javafx/vues/VuePartieLocaleFX.java
+++ src/main/java/Vitesse_javafx/vues/VuePartieLocaleFX.java
@@ -1,28 +1,19 @@
 package Vitesse_javafx.vues;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.ResourceBundle;
-
 import commun.debogage.DoitEtre;
 import commun.debogage.J;
 import commun_client.commandes.FabriqueCommande;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
@@ -31,12 +22,7 @@ import Vitesse_client.commandes.jouer_coup.JouerCoupPourEnvoi;
 import Vitesse_client.commandes.retour_accueil.RetourAccueil;
 import Vitesse_client.commandes.retour_accueil.RetourAccueilPourEnvoi;
 import Vitesse_client.vues.VuePartieLocale;
-import Vitesse_javafx.vues.composants.MainJoueur;
-import Vitesse_javafx.vues.composants.Paquet;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-
 public class VuePartieLocaleFX implements VuePartieLocale, Initializable {
 	
 	@FXML
@@ -44,20 +30,31 @@ public class VuePartieLocaleFX implements VuePartieLocale, Initializable {
 	
 	
 	@FXML
-	private Button jouerCarte1, jouerCarte2, jouerCarte3, jouerCarte4, jouerCarte5, boutonRetour;
+	private Button boutonRetour;
 	
 	private JouerCoupPourEnvoi jouerCoup;
 
 	private RetourAccueilPourEnvoi retourAccueil;
 	
 	public Rectangle carte01, carte02, carte03, carte04, carte05, carte06;
+	
+	private int numc1 = randomCard();
+	private int numc2 = randomCard();
+	private int numc3 = randomCard();
+	private int numc4 = randomCard();
+	private int numc5 = randomCard();
+	
+	private static ArrayList<Integer> carteUtilise = new ArrayList<Integer>();
+	int nombreCartes = 0;
 
-	Image imgcarte01 = new Image(randomCardSource());
-	Image imgcarte02 = new Image(randomCardSource());
-	Image imgcarte03 = new Image(randomCardSource());
-	Image imgcarte04 = new Image(randomCardSource());
-	Image imgcarte05 = new Image(randomCardSource());
-	Image imgcarte06 = new Image(randomCardSource());
+	Image imgcarte01 = new Image(intToStringImg(numc1));
+	Image imgcarte02 = new Image(intToStringImg(numc2));
+	Image imgcarte03 = new Image(intToStringImg(numc3));
+	Image imgcarte04 = new Image(intToStringImg(numc4));
+	Image imgcarte05 = new Image(intToStringImg(numc5));
+	Image imgcarte06 = new Image("images/Gray_back.jpg");
+	
+	
 	
 
 	@Override
@@ -66,12 +63,8 @@ public class VuePartieLocaleFX implements VuePartieLocale, Initializable {
 		
 		DoitEtre.nonNul(texteTmpPartieLocale);
 		DoitEtre.nonNul(texteNombreCoups);
-		DoitEtre.nonNul(jouerCarte1);
-		DoitEtre.nonNul(jouerCarte2);
-		DoitEtre.nonNul(jouerCarte3);
-		DoitEtre.nonNul(jouerCarte4);
-		DoitEtre.nonNul(jouerCarte5);
 		DoitEtre.nonNul(boutonRetour);
+		
 		carte01.setFill(new ImagePattern(imgcarte01));
 		carte02.setFill(new ImagePattern(imgcarte02));
 		carte03.setFill(new ImagePattern(imgcarte03));
@@ -94,56 +87,131 @@ public class VuePartieLocaleFX implements VuePartieLocale, Initializable {
 	public void installerCapteursEvenementsUsager() {
 		J.appel(this);
 		
-		jouerCarte1.setOnAction(new EventHandler<ActionEvent>() {
+		carte01.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
-			public void handle(ActionEvent event) {
+			public void handle(MouseEvent event) {
 				J.appel(this);
 				
-				jouerCoup.setInfoCoup(extraireInfoCoup());
+				jouerCoup.setInfoCoup(numc1);
+				
+				imgcarte06 = new Image(intToStringImg(numc1));
+				
+				carte06.setFill(new ImagePattern(imgcarte06));
+				
+				//Changer carte actuelle
+				if(nombreCartes != 52) {
+					numc1 = randomCard();
+				
+					imgcarte01 = new Image(intToStringImg(numc1));
+				
+					carte01.setFill(new ImagePattern(imgcarte01));
+					
+				}
 				
 				jouerCoup.envoyerCommande();
 			}
 		});
 		
-		jouerCarte2.setOnAction(new EventHandler<ActionEvent>() {
+		carte02.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
-			public void handle(ActionEvent event) {
+			public void handle(MouseEvent event) {
 				J.appel(this);
 				
-				jouerCoup.setInfoCoup(extraireInfoCoup());
+				jouerCoup.setInfoCoup(numc2);
+				
+				imgcarte06 = new Image(intToStringImg(numc2));
+				
+				carte06.setFill(new ImagePattern(imgcarte06));
+				
+				//Changer carte actuelle
+				
+				if(nombreCartes != 52) {
+					numc2 = randomCard();
+				
+					imgcarte02 = new Image(intToStringImg(numc2));
+				
+					carte02.setFill(new ImagePattern(imgcarte02));
+					
+				}
 				
 				jouerCoup.envoyerCommande();
 			}
 		});
 		
-		jouerCarte3.setOnAction(new EventHandler<ActionEvent>() {
+		carte03.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
-			public void handle(ActionEvent event) {
+			public void handle(MouseEvent event) {
 				J.appel(this);
 				
-				jouerCoup.setInfoCoup(extraireInfoCoup());
+				jouerCoup.setInfoCoup(numc3);
+				
+				imgcarte06 = new Image(intToStringImg(numc3));
+				
+				carte06.setFill(new ImagePattern(imgcarte06));
+				
+				//Changer carte actuelle
+				
+				if(nombreCartes != 52) {
+					numc3 = randomCard();
+				
+					imgcarte03 = new Image(intToStringImg(numc3));
+				
+					carte03.setFill(new ImagePattern(imgcarte03));
+					
+				}
+				
+				jouerCoup.envoyerCommande();
+				
+			}
+		});
+		
+		carte04.setOnMouseClicked(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				J.appel(this);
+				
+				jouerCoup.setInfoCoup(numc4);
+				
+				imgcarte06 = new Image(intToStringImg(numc4));
+				
+				carte06.setFill(new ImagePattern(imgcarte06));
+				
+				//Changer carte actuelle
+				
+				if(nombreCartes != 52) {
+					numc4 = randomCard();
+				
+					imgcarte04 = new Image(intToStringImg(numc4));
+				
+					carte04.setFill(new ImagePattern(imgcarte04));
+					
+				}
 				
 				jouerCoup.envoyerCommande();
 			}
 		});
 		
-		jouerCarte4.setOnAction(new EventHandler<ActionEvent>() {
+		carte05.setOnMouseClicked(new EventHandler<MouseEvent>() {
 			@Override
-			public void handle(ActionEvent event) {
+			public void handle(MouseEvent event) {
 				J.appel(this);
 				
-				jouerCoup.setInfoCoup(extraireInfoCoup());
+				jouerCoup.setInfoCoup(numc5);
 				
-				jouerCoup.envoyerCommande();
-			}
-		});
-		
-		jouerCarte5.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				J.appel(this);
+				imgcarte06 = new Image(intToStringImg(numc5));
 				
-				jouerCoup.setInfoCoup(extraireInfoCoup());
+				carte06.setFill(new ImagePattern(imgcarte06));
+				
+				//Changer carte actuelle
+				
+				if(nombreCartes != 52) {
+					numc5 = randomCard();
+				
+					imgcarte05 = new Image(intToStringImg(numc5));
+				
+					carte05.setFill(new ImagePattern(imgcarte05));
+					
+				}
 				
 				jouerCoup.envoyerCommande();
 			}
@@ -174,14 +242,24 @@ public class VuePartieLocaleFX implements VuePartieLocale, Initializable {
 	public void afficherNombreCoups(int nombreCoups) {
 		J.appel(this);
 		
-		texteNombreCoups.setText(String.valueOf(nombreCoups));
+		texteNombreCoups.setText(String.valueOf(nombreCoups - 5));
 	}
 	
-	public String randomCardSource() {
-		int signeTemp=(int)(Math.random() * (52));
+	public String intToStringImg(int signeTemp) {
 		String signe = String.valueOf(signeTemp);
 		String lienImage = "/images/" + signe + ".jpg";
 		return lienImage;
+	}
+	
+	public int randomCard() {
+		int temp;
+		do {
+			temp = (int)(Math.random() * (52));
+		} while (carteUtilise.contains(temp));
+		
+		carteUtilise.add(temp);
+		nombreCartes++;
+		return temp;
 	}
 
 }
